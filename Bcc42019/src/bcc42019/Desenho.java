ad0beaa44d53ea7d230f394f99d51c73cd69f5ff
Mr.Schneider <43351859+1MrSchneider@users.noreply.github.com>
2019-08-05 17:04:09 -0300

    Add files via upload

---
+++ Bcc42019/src/bcc42019/Desenho.java
@@ -0,0 +1,14 @@
+package bcc42019;
+import java.awt.Graphics;
+import javax.swing.JApplet;
+
+public class Desenho extends JApplet{
+    public void paint(Graphics g){
+        g.drawLine(20, 20, 150, 150);
+        g.drawLine(150, 20, 20, 150);
+        
+        
+        
+    }
+    
+}
